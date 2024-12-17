c45dfaf170c319eb7722a6e33e677e72d13f958e
evalin1995 <nhglje250@gmail.com>
2024-12-17 15:36:14 +0800

    1217deploy-3

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom"
 export default function Navbar() {
     return (
         <div>
-            <ul class="menu">
+            <ul className="menu">
                 <li><Link to="/">HOME</Link>                </li>
                 <li><Link to="/about">ABOUT</Link></li>
                 <li><Link to="/news">                NEWS</Link></li>
-                <li><a href="public\html\masonry.html">Mount Masonry</a></li>
-                <li><a href="public\html\carousel.html">NEW Curry</a></li>
-                <li><a href="public\html\cycle.html">Mount Cycle</a></li>
+                <li><a href="/html/masonry.html">Mount Masonry</a></li>
+                <li><a href="/html/carousel.html">NEW Curry</a></li>
+                <li><a href="/html/cycle.html">Mount Cycle</a></li>
                 </ul>
         </div>
     )
