729dc7fd4c824eeccdfe5f262ab94a3636a8d5f8
evalin1995 <nhglje250@gmail.com>
2024-12-17 15:51:58 +0800

    1207deploy-5

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,9 +7,9 @@ export default function Navbar() {
                 <li><Link to="/">HOME</Link>                </li>
                 <li><Link to="/about">ABOUT</Link></li>
                 <li><Link to="/news">                NEWS</Link></li>
-                <li><a href="/html/masonry.html">Mount Masonry</a></li>
-                <li><a href="/html/carousel.html">NEW Curry</a></li>
-                <li><a href="/html/cycle.html">Mount Cycle</a></li>
+                <li><a href="./html/masonry.html">Mount Masonry</a></li>
+                <li><a href="./html/carousel.html">NEW Curry</a></li>
+                <li><a href="./html/cycle.html">Mount Cycle</a></li>
                 </ul>
         </div>
     )
